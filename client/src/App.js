d7a0c157125e59b551e770999d3ab86c362045d8
Paweł Szybaj <pawelszybaj@wp.pl>
2019-05-26 23:36:32 +0200

    Private Routes

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,8 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
+import Dashborad from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 
 const App = () => {
   if (localStorage.token) {
@@ -32,6 +34,7 @@ const App = () => {
           <Switch>
             <Route exact path='/register' component={Register} />
             <Route exact path='/login' component={Login} />
+            <PrivateRoute exact path='/dashboard' component={Dashborad} />
           </Switch>
         </section>
       </Router>
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,7 +1,34 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { logout } from '../../actions/authActions';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-const Navbar = () => {
+const Navbar = ({ auth: { isAuthenticated, isLoading }, logout }) => {
+  const authLinks = (
+    <ul>
+      <li>
+        <NavLink onClick={logout} to='#!'>
+          <i className='fas fa-sign-out-alt' />{' '}
+          <span className='hide-sm'>Logout</span>
+        </NavLink>
+      </li>
+    </ul>
+  );
+
+  const guestLinks = (
+    <ul>
+      <li>
+        <NavLink to='/#!'>Developers</NavLink>
+      </li>
+      <li>
+        <NavLink to='/register'>Register</NavLink>
+      </li>
+      <li>
+        <NavLink to='/login'>Login</NavLink>
+      </li>
+    </ul>
+  );
   return (
     <nav className='navbar bg-dark'>
       <h1>
@@ -9,19 +36,21 @@ const Navbar = () => {
           <i className='fas fa-code' /> DevConnector
         </NavLink>
       </h1>
-      <ul>
-        <li>
-          <NavLink to='/profiles.html'>Developers</NavLink>
-        </li>
-        <li>
-          <NavLink to='/register'>Register</NavLink>
-        </li>
-        <li>
-          <NavLink to='/login'>Login</NavLink>
-        </li>
-      </ul>
+      {!isLoading && <>{isAuthenticated ? authLinks : guestLinks}</>}
     </nav>
   );
 };
 
-export default Navbar;
+Navbar.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  { logout }
+)(Navbar);
